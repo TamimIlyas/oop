0bfefbdd942796cb5d2a538404a2a340930cfc3d
TamimIlyas <75974165+TamimIlyas@users.noreply.github.com>
2021-09-28 16:49:47 +0200

    OOP

--- OOP/src/Klasse.java
+++ OOP/src/Klasse.java
@@ -1,3 +1,12 @@
-public class Klasse {
+class klasse {
+    int x;
 
+    public static void main(String[] args) {
+        klasse myObj1 = new klasse();
+        klasse myObj2 = new klasse();
+        myObj1.x = 200;
+        myObj2.x = 200;
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
 }
